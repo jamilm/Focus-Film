1723737a5244fa20e0b3add2dcee34de9e65b11d
Jamil Merali <jamil@mera.li>
2016-04-02 13:42:11 -0400

    Init

---
+++ server.py
@@ -0,0 +1,115 @@
+import argparse
+import math
+import time
+import threading
+import os
+
+from pythonosc import dispatcher
+from pythonosc import osc_server
+
+def initArrays():
+  global alphaArray, betaArray, deltaArray, thetaArray, concentrationArray
+  global timesCalled, lastSubmit
+  timesCalled = 0
+  alphaArray = []
+  betaArray = []
+  thetaArray = []
+  deltaArray = []
+  concentrationArray = []
+  lastSubmit = time.time()
+  serverRunning = False
+
+def print_volume_handler(unused_addr, args, volume):
+  print("[{0}] ~ {1}".format(args[0], volume))
+
+def print_compute_handler(unused_addr, args, volume):
+  print("Ok")
+  try:
+    print("[{0}] ~ {1}".format(args[0], args[1](volume)))
+  except ValueError: pass
+
+def getAverage(*nums):
+  total = 0
+  nums = list(nums)
+  for i in range(len(nums[0])):
+    if i>0:
+      total += float(nums[0][i])
+  total = total/4
+  return total
+
+def arrayAverage(array):
+  total = 0
+  for i in array:
+    total += i
+  return total/(len(array))
+
+def alphaData(*data):
+  global alphaArray
+  alphaArray.append(getAverage(data))
+  submitData()
+
+def betaData(*data):
+  global betaArray
+  betaArray.append(getAverage(data))
+  submitData()
+
+def deltaData(*data):
+  global deltaArray
+  deltaArray.append(getAverage(data))
+  submitData()
+
+def thetaData(*data):
+  global thetaArray
+  thetaArray.append(getAverage(data))
+  submitData()
+
+def concentrationData(*data):
+  global concentrationArray
+  concentrationArray.append(getAverage(data))
+  submitData()
+
+def submitData():
+  global lastSubmit, alphaArray, betaArray, deltaArray, thetaArray, concentrationArray
+  if time.time() - lastSubmit > 3:
+    lastSubmit = time.time()
+    avgArray = [arrayAverage(alphaArray), arrayAverage(betaArray), arrayAverage(deltaArray), arrayAverage(thetaArray), arrayAverage(concentrationArray)]
+    print(avgArray)
+    mostActiveIndex = avgArray.index(max(avgArray))
+    f = open('./file.json', 'a')
+    f.write(str(mostActiveIndex)+",")
+    f.close()
+    # print('Wrote ' + str(mostActiveIndex)) + ' to file.')
+    alphaArray = []
+    betaArray = []
+    thetaArray = []
+    deltaArray = []
+    concentrationData = []
+
+def endServer():
+  global server
+  server.shutdown()
+
+def runMuseServer():
+  global dispatcher, Dispatcher, server
+  parser = argparse.ArgumentParser()
+  parser.add_argument("--ip",
+      default="localhost", help="The ip to listen on")
+  parser.add_argument("--port",
+      type=int, default=1337, help="The port to listen on")
+  args = parser.parse_args()
+
+  dispatcher = dispatcher.Dispatcher()
+  dispatcher.map("/muse/elements/alpha_relative", alphaData)
+  dispatcher.map("/muse/elements/beta_relative", betaData)
+  dispatcher.map("/muse/elements/delta_relative", deltaData)
+  dispatcher.map("/muse/elements/theta_relative", thetaData)
+  dispatcher.map("/muse/elements/jaw_clench", concentrationData)
+  	
+  server = osc_server.ThreadingOSCUDPServer((args.ip, args.port), dispatcher)
+  print("Serving on {}".format(server.server_address))
+  server_thread = threading.Thread(target=server.serve_forever)
+  server_thread.start()
+
+
+initArrays()
+runMuseServer()
